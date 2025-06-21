6407928555599e09f915d687ec46eb1675baede1
kalyansDomain <kalyan.reddy@winitsoftware.com>
2025-06-21 21:24:20 +0530

    Changes for Final Deployement

--- public/search.js
+++ public/search.js
@@ -544,3 +544,14 @@ function handleBackNavigation(element, event) {
         toggle.click(); // This simulates opening the main menu
     }
 }
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+    if (
+      e.keyCode == 123 || // F12
+      (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || // Ctrl+Shift+I
+      (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || // Ctrl+Shift+J
+      (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) // Ctrl+U
+    ) {
+      return false;
+    }
+  }
